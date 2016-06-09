b447c98ca8a166b674bc9fa257d219d6e7d87119
Zak Buys <zakbuys85@gmail.com>
2016-06-09 16:05:20 +0100

    coloured circle moves with mouse!!!!

--- public/index.js
+++ public/index.js
@@ -4,27 +4,27 @@ window.onload = function() {
     console.log(canvas);
 
     var context = canvas.getContext('2d');
-    context.shadowBlur = 50;
-    context.shadowColor = 'black';
+    // context.shadowBlur = 50;
+    // context.shadowColor = 'black';
     // context.fillStyle = "red";
-    var grd=context.createLinearGradient(0,0,400,0);
-    grd.addColorStop(0,"black");
-    grd.addColorStop(1,"white");
+    // var grd=context.createLinearGradient(0,0,400,0);
+    // grd.addColorStop(0,"black");
+    // grd.addColorStop(1,"white");
 
     // context.fillStyle=grd;
     // context.fillRect(50, 50, 500, 200);
     // context.clearRect(45,45,60,60);
     // context.strokeRect(50,50,50,50);
 
-    context.beginPath();
-    context.moveTo( 100, 100 );
-    context.lineTo(100, 150 );
-    context.lineTo(150, 150 );
-    context.lineTo(250, 200 );
-    context.lineTo(450, 200 );
-    context.lineTo(450, 100 );
-    context.lineTo(150, 100 );
-    context.stroke();
+    // context.beginPath();
+    // context.moveTo( 100, 100 );
+    // context.lineTo(100, 150 );
+    // context.lineTo(150, 150 );
+    // context.lineTo(250, 200 );
+    // context.lineTo(450, 200 );
+    // context.lineTo(450, 100 );
+    // context.lineTo(150, 100 );
+    // context.stroke();
 
 //     var gradient = context.createRadialGradient(100,100,100,100,100,0);
 // gradient.addColorStop(0,"white");
@@ -51,17 +51,70 @@ window.onload = function() {
     // context.arc(300,150,50,0,2*Math.PI);
     // context.stroke();
 
-    var drawCircle = function(x, y) {
-    context.beginPath();
-    context.arc(x-340,y-70,50,0,2*Math.PI);
-    context.stroke();
-};
+    var canvasPos = getPosition(canvas);
+    var mouseX = 0;
+    var mouseY = 0;
 
-    canvas.onclick = function(event) {
-        console.log(event);
+    // canvas.addEventListener("mousemove", setMousePosition, false);
+    //
+    // var setMousePosition = function(e) {
+    //     mouseX = e.clientX - canvasPos.x;
+    //     mouseY = e.clientY - canvasPos.y;
+    // };
 
-        drawCircle(event.x, event.y);
 
+
+    // var drawCircle = function() {
+    // context.clearRect(0,0, canvas.width, canvas.height);
+    // context.beginPath();
+    // context.arc(mouseX,mouseY,50,0,2*Math.PI, true);
+    // context.fillStyle ="red";
+    // context.fill();
+    //
+    // requestAnimationFrame(drawCircle);
+    // };
+    //
+    // drawCircle();
+
+    function getPosition(el) {
+      var xPosition = 0;
+      var yPosition = 0;
+
+      while (el) {
+        xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+        yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+        el = el.offsetParent;
+      }
+      return {
+        x: xPosition,
+        y: yPosition
+      };
+    }
+
+
+    // var dc = drawCircle(x,y,z);
+
+    // var reDrawCircle = function(cd) {
+    //
+    // };
+
+
+    // canvas.onclick = function(event) {
+    //     console.log(event);
+    //     // drawCircle(event.x, event.y);
+    // };
+
+    canvas.onmousemove = function(e) {
+
+        mouseX = e.clientX - canvasPos.x;
+        mouseY = e.clientY - canvasPos.y;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        context.beginPath();
+        context.arc(mouseX, mouseY, 50, 0, 2 * Math.PI, true);
+        context.fillStyle = "#FF6A6A";
+        context.fill();
     };
 
 
